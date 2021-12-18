747547646e37c4272565e8c84d27829f0fe1a394
Manoranjith Dhanasekar <60169497+Manoranjith-crypto@users.noreply.github.com>
2021-12-18 21:04:35 +0530

    Create SoluToken.sol

---
+++ SoluToken.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.0;
+
+import "./ERC20.sol";
+
+contract SoluToken is ERC20 {
+    string public constant symbol = "ST";
+    string public constant name = "Solulab Token";
+    uint8 public constant decimals = 18;
+    
+    uint private constant __totalSupply = 100000000; //100 million token as a total supply
+    mapping (address => uint) private __balanceOf;
+    mapping (address => mapping (address => uint)) private __allowances;
+    
+    constructor() public {
+            __balanceOf[msg.sender] = __totalSupply; //asigning totalsupply to balanceof
+    }
+    
+    function totalSupply() public constant returns (uint _totalSupply) {
+        _totalSupply = __totalSupply;
+    }
+    
+    function balanceOf(address _addr) public constant returns (uint balance) {
+        return __balanceOf[_addr]; //shows the current balance
+    }
+    
+    function transfer(address _to, uint _value) public returns (bool success) {
+        if (_value > 0 && _value <= balanceOf(msg.sender)) {
+            __balanceOf[msg.sender] -= _value;
+            __balanceOf[_to] += _value;
+            return true; // checking the transfer was success or not
+        }
+        return false;
+
+        
+    }
+    
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+        if (__allowances[_from][msg.sender] > 0 &&
+            _value > 0 &&
+            __allowances[_from][msg.sender] >= _value && 
+            __balanceOf[_from] >= _value) {
+            __balanceOf[_from] -= _value;
+            __balanceOf[_to] += _value;
+            
+            __allowances[_from][msg.sender] -= _value;
+            return true;
+        }
+        return false;
+    }
+    
+    function approve(address _spender, uint _value) public returns (bool success) {
+        __allowances[msg.sender][_spender] = _value;
+        return true;
+    }
+    
+    function allowance(address _owner, address _spender) public constant returns (uint remaining) {
+        return __allowances[_owner][_spender];
+    }
+}
